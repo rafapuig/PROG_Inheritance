5076be367fd8a463bd28164d0a57d718547e66ea
Rafa Puig <rafapuig.profe@gmail.com>
2025-01-25 17:51:52 +0100

    Sealed

--- src/disabling/SealedDemo.java
+++ src/disabling/SealedDemo.java
@@ -10,5 +10,15 @@ final class Password extends Security {}
 // Lock permite que otras clases deriven o extiendan de ella
 non-sealed class Lock extends Security {}
 
+
+// Alternativamente, podemos especificar todas las subclases dentro de la clase sellada
+// El compilador asume que las clases definidas dentro de la clase sellada conforman la lista de subclases
+abstract sealed class SecurityAlt {
+    final class Password extends SecurityAlt {}
+    non-sealed class Lock extends SecurityAlt {}
+}
+
 public class SealedDemo {
 }
+
+
