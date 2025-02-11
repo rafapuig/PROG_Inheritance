ee5918ae95c9e973c50835af7720e1ed41debd9c
Rafa Puig <rafapuig.profe@gmail.com>
2025-02-11 15:18:03 +0100

    Reubicacion de paquetes

--- src/exercises/composition/exercise2/CompositionExercise2.java
+++ src/exercises/composition/exercise2/CompositionExercise2.java
@@ -118,6 +118,38 @@ class Polygon {
 public class CompositionExercise2 {
 
     public static void main(String[] args) {
+
+        // Forma verbosa de crear el polígono
+
+        // Crear cada punto y guardar una referencia en una variable
+        Point p1 = new Point(0, 0);
+        Point p2 = new Point(2, 0);
+        Point p3 = new Point(2, 2);
+        Point p4 = new Point(0, 2);
+
+        // Crear el objeto array de referencias a objetos de tipo Punto
+        Point[] points = new Point[4]; // Cuatro referencias a 4 objetos Punto
+        points[0] = p1; //Copiar en la posición 0 del array la referencia al punto referenciado por la variable p1
+        points[1] = p2;
+        points[2] = p3;
+        points[3] = p4;
+
+        // Crear el polígono pasando la referencia al objeto array de puntos almacenada en la variable points
+        Polygon polygon2 = new Polygon(points);
+
+
+        // Forma algo menos verbosa ...
+        Point p5 = new Point(0, 0);
+        Point p6 = new Point(2, 0);
+        Point p7 = new Point(2, 2);
+        Point p8 = new Point(0, 2);
+
+        //Creamos el array mediante las referencias a los objetos punto
+        Point[] points2 = new Point[] {p5, p6, p7, p8};
+        Polygon polygon3 = new Polygon(points2);
+
+
+        // Forma más compacta de crear el polígono
         Polygon polygon = new Polygon(new Point[]{
                 new Point(0, 0),
                 new Point(2, 0),
